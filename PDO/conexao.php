25f6f90c84ef507740f26231d96d360d064df9c1
RafaCarvalh0 <rafacarvalho601@gmail.com>
2026-03-24 09:31:55 -0300

    ajustes

--- PDO/conexao.php
+++ PDO/conexao.php
@@ -5,4 +5,4 @@ $pdo = new PDO('sqlite:' . $caminhoBanco);
 
 echo 'Conectei';
 
-$pdo->exec('CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);');
+$pdo->exec('CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);');

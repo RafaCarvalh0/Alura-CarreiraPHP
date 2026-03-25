ab44ce1fadb4955efb5fda5978bf169d271e778c
RafaCarvalh0 <rafacarvalho601@gmail.com>
2026-03-25 13:40:12 -0300

    main

--- PDO/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$caminhoBanco = __DIR__ . '/banco.sqlite';
-$pdo = new PDO('sqlite:' . $caminhoBanco);
-
-echo 'Conectei';
-
-$pdo->exec('CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);');

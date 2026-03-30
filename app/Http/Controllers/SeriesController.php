e876ce0ebdbc842b740b6f9dac37772014748e48
RafaCarvalh0 <rafacarvalho601@gmail.com>
2026-03-30 20:51:51 -0300

    ajustes

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
+use App\Mail\SeriesCreated;
 use App\Models\Series;
 use App\Models\User;
 use App\Repositories\SeriesRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
-use \App\Mail\SeriesCreated;
 
 class SeriesController extends Controller
 {
@@ -34,18 +35,12 @@ class SeriesController extends Controller
     public function store(SeriesFormRequest $request)
     {
         $serie = $this->repository->add($request);
-
-        $userList = User::all();
-        foreach ($userList as $index => $user) {
-            $email = new SeriesCreated(
-                $serie->nome,
-                $serie->id,
-                (int) $request->seasonsQty,
-                (int) $request->episodesPerSeason,
-            );
-            $when = now()->addSeconds($index * 5);
-            Mail::to($user)->later($when, $email);
-        }
+        \App\Events\SeriesCreated::dispatch(
+            $serie->nome,
+            $serie->id,
+            $request->seasonsQty,
+            $request->episodesPerSeason,
+        );
 
         return to_route('series.index')
             ->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
---
+++ app/Listeners/LogSeriesCreated.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\SeriesCreated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+
+class LogSeriesCreated implements ShouldQueue
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function handle(SeriesCreated $event)
+    {
+        Log::info("Série {$event->seriesName} criada com sucesso");
+    }
+}
